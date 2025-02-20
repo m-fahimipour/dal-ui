89ab29e5d5eb497fa42a3953278d4b57a6b0cdb6
m-fahimipour <mostafa.fahimipour@gmail.com>
2025-02-20 19:39:36 +0330

    update: variants type for typography was updated

--- libs/design-system/src/types/components/typography/Typography.d.ts
+++ libs/design-system/src/types/components/typography/Typography.d.ts
@@ -2,6 +2,8 @@ import type { HTMLAttributes, ReactNode } from "react";
 
 type TComponent = "p" | "span" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
+interface ITypographyVariants {}
+
 type TBaseElement<T extends TComponent> = T extends "p"
   ? HTMLParagraphElement
   : T extends "span"
@@ -15,7 +17,9 @@ export interface ITypography<T extends TComponent = "span">
   extends IBaseTypography<T> {
   component?: T;
   children?: ReactNode | ReactNode[];
-  variants?: string;
+  variants?: keyof ITypographyVariants extends never
+    ? string
+    : keyof ITypographyVariants;
 }
 
 declare const Typography: <T extends TComponent>(
