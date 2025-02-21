328b6f71ee37c2cdf54ce161e9f0c44bc88dfe1b
m-fahimipour <mostafa.fahimipour@gmail.com>
2025-02-21 19:37:37 +0330

    feat: typography component was completed

--- libs/design-system/src/components/typography/Typography.tsx
+++ libs/design-system/src/components/typography/Typography.tsx
@@ -1,12 +1,24 @@
 import { createElement } from "react";
 
+import { twJoin } from "tailwind-merge";
+
 import type { ITypography } from "../../types/components/typography/Typography";
 
 function Typography({
   component = "span",
   children,
+  variants,
+  className,
+  ...rest
 }: ITypography): JSX.Element {
-  return createElement(component, {}, children);
+  return createElement(
+    component,
+    {
+      ...rest,
+      className: twJoin("Dalui-typography-root", className, variants),
+    },
+    children
+  );
 }
 
 export default Typography;
