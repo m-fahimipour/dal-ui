61932b801ab33310b955ca29bfc41f381a0e56e2
m-fahimipour <mostafa.fahimipour@gmail.com>
2025-02-27 00:16:43 +0330

    feat: add terser-webpack-plugin

--- libs/design-system/webpack.config.prod.cjs
+++ libs/design-system/webpack.config.prod.cjs
@@ -1,4 +1,16 @@
+const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
   extends: "./webpack.config.base.cjs",
   mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      terserOptions: {
+        format: {
+          comments: false,
+        },
+      },
+      extractComments: false,
+    })],
+  },
 };
