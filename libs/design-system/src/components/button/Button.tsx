05b0beb0b4dceaa6a5dc1f100bc6cc3cd4fa1d1a
Mostafa <mostafa.fahimipour@gmail.com>
2025-02-26 18:00:03 +0330

    feat: add loader to button component

--- libs/design-system/src/components/button/Button.tsx
+++ libs/design-system/src/components/button/Button.tsx
@@ -1,6 +1,8 @@
-import { twMerge } from "tailwind-merge";
+import { twJoin, twMerge } from "tailwind-merge";
 
 import type { IButtonProps } from "../../types/components/button/Button";
+import { Dot1 } from "./components/loading/dot/dot-1/Dot1";
+import { Spinner1 } from "./components/loading/spinner/spinner-1/Spinner1";
 import RippleEffect from "./components/RippleEffect";
 
 function Button({
@@ -10,6 +12,7 @@ function Button({
   variant = "btn-contained",
   startIcon,
   endIcon,
+  isLoading,
   hasRippleEffect = true,
   rippleProps,
   ...otherProps
@@ -19,17 +22,47 @@ function Button({
       {...otherProps}
       className={twMerge(
         "Dui-Button-root",
-        "relative inline-flex cursor-pointer items-center justify-center overflow-hidden rounded-full px-4 py-1",
+        "relative inline-flex cursor-pointer items-center justify-center overflow-hidden rounded-full px-4 py-1 transition-colors",
         startIcon || endIcon ? "gap-2" : "",
+        isLoading && "text-transparent",
         className,
         [variant, size],
       )}
     >
-      {startIcon}
+      {startIcon && (
+        <span
+          className={twJoin(
+            "Dui-startIcon-root transition-opacity",
+            isLoading && "opacity-0",
+          )}
+        >
+          {startIcon}
+        </span>
+      )}
 
       {children}
 
-      {endIcon}
+      {endIcon && (
+        <span
+          className={twJoin(
+            "Dui-endIcon-root transition-opacity",
+            isLoading && "opacity-0",
+          )}
+        >
+          {endIcon}
+        </span>
+      )}
+
+      {isLoading && (
+        <div
+          className={twJoin(
+            "absolute inset-0 flex items-center justify-center",
+          )}
+        >
+          {/* <Spinner1 /> */}
+          <Dot1 />
+        </div>
+      )}
 
       {hasRippleEffect && (
         <RippleEffect
