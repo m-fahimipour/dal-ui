066074c1eac3cb232e46be9ed04ece561dbcbb21
Mostafa <mostafa.fahimipour@gmail.com>
2025-03-11 12:49:29 +0330

    udpate: global type was updated

--- libs/design-system/src/types/components/global.d.ts
+++ libs/design-system/src/types/components/global.d.ts
@@ -1,3 +1,4 @@
+import type { JSX } from "react";
 export type TFilterFalseValue<T extends object> = {
   [key in keyof T as T[key] extends true ? key : never]: T[key];
 };
@@ -12,10 +13,10 @@ interface IDefaultBreakpoints {
   "max-md": true;
   "max-lg": true;
   "max-xl": true;
-  "max-xxl": false;
+  "max-xxl": true;
 }
 
-export interface IBreakpoints extends IDefaultBreakpoints{}
+export interface IBreakpoints extends IDefaultBreakpoints {}
 
 export type TExtractStringKeys<T extends object> = {
   [key in keyof T]: key extends string ? key : never;
@@ -23,12 +24,18 @@ export type TExtractStringKeys<T extends object> = {
 
 export type TAdaptiveBreakpointsObject<
   IValues extends object,
-  Breakpoints extends object = IBreakpoints
+  Breakpoints extends object = IBreakpoints,
 > = {
   [key in keyof TFilterFalseValue<Breakpoints>]: keyof TFilterFalseValue<IValues>;
 };
 
 export type TAdaptiveBreakpointsArray<
   IValues extends object,
-  Breakpoints extends object = IBreakpoints
+  Breakpoints extends object = IBreakpoints,
 > = `${TExtractStringKeys<Breakpoints>}:${TExtractStringKeys<TFilterFalseValue<IValues>>}`[];
+
+export type TElementType<T extends keyof HTMLElementTagNameMap> =
+  HTMLElementTagNameMap[T];
+
+export type TElementAttributes<T extends keyof JSX.IntrinsicElements> =
+  JSX.IntrinsicElements[T];
