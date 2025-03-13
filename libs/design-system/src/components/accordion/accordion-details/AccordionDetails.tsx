4ee0b715a98350159ea775c9a28cf0cf6b6e2ea3
m-fahimipour <mostafa.fahimipour@gmail.com>
2025-03-13 16:21:32 +0330

    update: accordion-details component was completed

--- libs/design-system/src/components/accordion/accordion-details/AccordionDetails.tsx
+++ libs/design-system/src/components/accordion/accordion-details/AccordionDetails.tsx
@@ -1,18 +1,28 @@
+import { twJoin } from "tailwind-merge";
+
 import type { JSX } from "react";
 
+import type { TAccordionDetailsProps } from "../../../types/components/accordion/accordion-details";
+import { Block } from "../../block";
 import { Collapse } from "../../collapse";
 import { useAccordionContext } from "../context/AccordionContext";
 import type { IValueAccordionContext } from "../context/type";
 
-function AccordionDetails(): JSX.Element {
-  const value:IValueAccordionContext = useAccordionContext();
+function AccordionDetails({
+  children,
+  ...rest
+}: TAccordionDetailsProps): JSX.Element {
+  const value: IValueAccordionContext = useAccordionContext();
 
   return (
-    <Collapse isCollapsed={Boolean(value.isExpanded)}>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
-      voluptatum amet voluptate quam, voluptatem eveniet ducimus eos. Quis
-      perspiciatis magnam optio ratione impedit tempore expedita! Eveniet
-      perferendis iusto voluptas similique.
+    <Collapse isCollapsed={Boolean(!value.isExpanded)}>
+      <Block
+        {...rest}
+        className={twJoin("Dui-AccordionDetails-root", rest.className)}
+        role={rest.role || "region"}
+      >
+        {children}
+      </Block>
     </Collapse>
   );
 }
---
+++ libs/design-system/src/types/components/accordion/accordion-details.d.ts
@@ -0,0 +1,6 @@
+import type { JSX } from "react";
+
+import type { TBlockProps } from "../block/Block";
+
+export type TAccordionDetailsProps = {} & TBlockProps;
+declare const AccordionDetails: () => JSX.Element;
