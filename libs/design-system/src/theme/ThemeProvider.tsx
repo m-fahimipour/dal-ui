b02604ffc1deea28e9db1478abd5ed8ed45bc463
Mostafa <mostafa.fahimipour@gmail.com>
2025-04-28 16:34:25 +0330

    update: theme provider and functionality was updated(add color-scheme style)

--- libs/design-system/src/theme/ThemeProvider.tsx
+++ libs/design-system/src/theme/ThemeProvider.tsx
@@ -4,15 +4,17 @@ function ThemeProvider({ children }: { children: ReactNode[] }) {
   useLayoutEffect(() => {
     if (window) {
       const isDarkTheme = window.matchMedia(
-        "(prefers-color-scheme:dark)"
+        "(prefers-color-scheme:dark)",
       ).matches;
       const localTheme = localStorage.getItem("theme");
 
       if ((isDarkTheme && localTheme !== "light") || localTheme == "dark") {
         document.documentElement.classList.add("dark");
+        document.documentElement.style.colorScheme = "dark";
         if (!localTheme) localStorage.setItem("theme", "dark");
       } else {
         document.documentElement.classList.remove("dark");
+        document.documentElement.style.colorScheme = "light";
         if (!localTheme) localStorage.setItem("theme", "light");
       }
     }
--- libs/design-system/src/utils/theme/index.ts
+++ libs/design-system/src/utils/theme/index.ts
@@ -3,9 +3,11 @@ export function changeTheme() {
     if (localStorage.getItem("theme") == "dark") {
       localStorage.setItem("theme", "light");
       document.documentElement.classList.remove("dark");
+      document.documentElement.style.colorScheme = "light";
     } else {
       localStorage.setItem("theme", "dark");
       document.documentElement.classList.add("dark");
+      document.documentElement.style.colorScheme = "dark";
     }
   }
 }
